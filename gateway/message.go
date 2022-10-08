066ef323867f7dc6ec7046b153e36502a3ed7617
FANG <1639579565@qq.com>
2022-10-08 23:48:27 +0800

    # 响应相关

--- gateway/message.go
+++ gateway/message.go
@@ -1,11 +1,57 @@
 package gateway
 
-type Result struct {
-	Succeed bool   `json:"succeed"`
-	Msg     string `json:"msg"`
-	Data    any    `json:"data"`
+import (
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/kataras/iris/v12"
+)
+
+// var (
+// 	unknownError = "未知错误, 请联系管理员或稍后重试"
+// )
+
+func (m *Manager) sendJson(ctx iris.Context, code int, v any) {
+	ctx.StatusCode(code)
+	ctx.JSON(v)
 }
 
-var (
-	unknownError = "未知错误, 请联系管理员或稍后重试"
-)
+func (m *Manager) sendSimpleMessage(ctx iris.Context, code int, errs ...error) {
+	if errs == nil {
+		m.sendJson(ctx, code, map[string]string{
+			"msg": "成功",
+		})
+		return
+	}
+	for _, err := range errs {
+		log.Printf("[%s] CODE(%d) ERROR : %+v\n", ctx.Path(), code, err)
+	}
+	var msg string
+	switch code {
+	case iris.StatusBadRequest:
+		msg = "请求解析失败"
+	case iris.StatusInternalServerError:
+		msg = "服务器内部错误，请联系管理员"
+	case iris.StatusOK:
+		msg = "请求成功"
+	case iris.StatusPreconditionFailed:
+		msg = "预处理失败"
+	case iris.StatusForbidden:
+		msg = "权限认证失败"
+	default:
+		msg = "未知错误"
+	}
+	m.sendJson(ctx, code, map[string]string{
+		"msg": msg,
+	})
+}
+
+func (m *Manager) sendValidateMessage(ctx iris.Context, err error) {
+	for _, err := range err.(validator.ValidationErrors) {
+		log.Printf("[%s] CODE(%d) ERROR : %+v\n", ctx.Path(), iris.StatusOK, err)
+	}
+	ctx.StatusCode(iris.StatusOK)
+	ctx.JSON(map[string]any{
+		"msg": "数据校验失败",
+	})
+}
